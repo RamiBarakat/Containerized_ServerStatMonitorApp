85c2ea43cf583c6dc6190227ec17552ad014f28e
Rami Barakat <77164176+RamiBarakat@users.noreply.github.com>
2023-09-06 10:58:34 +0300

    Update readstats.py
    Logging all the actions is done and stored in readstats.log

--- python/readstats.py
+++ python/readstats.py
@@ -1,46 +1,69 @@
 import mysql.connector
 import psutil
+import logging
 
+logging.basicConfig(filename='readstats.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-cpu_usage = float(psutil.cpu_percent(5))
-memory_usage = float(psutil.virtual_memory().percent)
-disk_usage =  float(psutil.disk_usage('/')[3])
+def log_functions(func):
+    def wrapper(*args, **kwargs):
+        function_name = func.__name__
+        logging.info(f"Function {function_name} started with args={args}, kwargs={kwargs}")
+        try:
+            result = func(*args, **kwargs)
+            logging.info(f"Function {function_name} completed successfully. Result: {result}")
+            return result
+        except Exception as e:
+            error_message = f"Function {function_name} failed with error: {str(e)}"
+            logging.error(error_message, exc_info=True)
+            raise e
 
+    return wrapper
 
-print(cpu_usage)
-print(memory_usage)
-print(disk_usage)
+@log_functions
+def get_system_stats():
+    cpu_usage = float(psutil.cpu_percent(5))
+    memory_usage = float(psutil.virtual_memory().percent)
+    disk_usage = float(psutil.disk_usage('/')[3])
+    logging.info(f"CPU Usage: {cpu_usage}%")
+    logging.info(f"Memory Usage: {memory_usage}%")
+    logging.info(f"Disk Usage: {disk_usage}%")
+    return cpu_usage, memory_usage, disk_usage
 
+@log_functions
+def connect_to_database():
+    try:
+        conn = mysql.connector.connect(user='root', password='root', host='mysql', port="3306", database='db')
+        logging.info("Connected to the database")
+        return conn
+    except Exception as e:
+        error_message = f"Database connection failed: {str(e)}"
+        logging.error(error_message)
+        raise e
 
+@log_functions
+def insert_data_into_table(conn, table_name, data):
+    try:
+        cursor = conn.cursor(buffered=True)
+        cursor.execute(f"INSERT INTO {table_name} (value) VALUES (%s)", (data,))
+        logging.info(f"Inserted data into {table_name} table")
+        conn.commit()
+        cursor.close()
+    except Exception as e:
+        error_message = f"Failed to insert data into {table_name} table: {str(e)}"
+        logging.error(error_message)
+        raise e
 
-conn = mysql.connector.connect(user='root', password='root', host='mysql', port="3306", database='db')
-print("DB connected")
+@log_functions
+def main():
+    cpu_usage, memory_usage, disk_usage = get_system_stats()
+    conn = connect_to_database()
 
+    insert_data_into_table(conn, "cpu", cpu_usage)
+    insert_data_into_table(conn, "mem", memory_usage)
+    insert_data_into_table(conn, "disk", disk_usage)
 
-cursor = conn.cursor(buffered=True)
+    conn.close()
+    logging.info("Closed the database connection")
 
-cursor.execute("SHOW TABLES;")
-print("man o man")
-
-
-data_to_insert = (cpu_usage,)
-print(data_to_insert)
-cursor.execute('INSERT INTO cpu (value) VALUES (%s)', data_to_insert)
-
-
-data_to_insert = (memory_usage,)
-cursor.execute('INSERT INTO mem (value) VALUES (%s)', data_to_insert)
-
-
-data_to_insert = (disk_usage,)
-cursor.execute('INSERT INTO disk (value) VALUES (%s)', data_to_insert)
-
-cursor.execute("SELECT * FROM cpu;")
-
-# Fetch and print the results (optional)
-for row in cursor.fetchall():
-    print(row)
-    print("mer")
-
-conn.commit()
-conn.close()
+if __name__ == "__main__":
+    main()
